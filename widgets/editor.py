933201477f9928a35983580e50ba0bc756d161c7
mirkobrombin <send@mirko.pm>
2022-01-23 16:35:44 +0100

    syntax highlight

--- widgets/editor.py
+++ widgets/editor.py
@@ -10,6 +10,33 @@ class Editor(Gtk.Paned):
 
     __dbg_color = "#fa973a"
     intellisense = Intellisense()
+    
+    source_scroll = Gtk.ScrolledWindow(vexpand=True, hexpand=True)
+    debug_scroll = Gtk.ScrolledWindow(vexpand=True, hexpand=True)
+    lang_manager = GtkSource.LanguageManager()
+    style_scheme_manager = GtkSource.StyleSchemeManager()
+    debug_buffer = Gtk.TextBuffer()
+    source_buffer = GtkSource.Buffer(
+        highlight_syntax=True,
+        highlight_matching_brackets=True,
+        style_scheme=style_scheme_manager.get_scheme("oblivion"),
+        language=lang_manager.get_language("yaml")
+    )
+    source_view = GtkSource.View(
+        wrap_mode=Gtk.WrapMode.WORD, 
+        auto_indent=True, 
+        indent_width=4, 
+        tab_width=4, 
+        indent_on_tab=True, 
+        show_line_numbers=True, 
+        insert_spaces_instead_of_tabs=True,
+        buffer=source_buffer
+    )
+    debug_view = Gtk.TextView(
+        wrap_mode=Gtk.WrapMode.WORD, 
+        editable=False,
+        buffer=debug_buffer
+    )
 
     def __init__(self, app):
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL)
@@ -18,28 +45,19 @@ class Editor(Gtk.Paned):
         self.build_widgets()
     
     def build_widgets(self):
-        self.text_scroll = Gtk.ScrolledWindow(vexpand=True, hexpand=True)
-        self.debug_scroll = Gtk.ScrolledWindow(vexpand=True, hexpand=True)
-        self.text_view = GtkSource.View(wrap_mode=Gtk.WrapMode.WORD, auto_indent=True, indent_width=4, tab_width=4, indent_on_tab=True, show_line_numbers=True, insert_spaces_instead_of_tabs=True)
-        self.debug_view = Gtk.TextView(wrap_mode=Gtk.WrapMode.WORD, editable=False)
-
-        self.text_view.get_completion().add_provider(self.intellisense)
-
+        self.source_view.get_completion().add_provider(self.intellisense)
         self.debug_view.get_style_context().add_class("debug-view")
         
-        self.text_buffer = self.text_view.get_buffer()
-        self.text_buffer.connect("changed", self.on_text_changed)
+        self.source_buffer.connect("changed", self.on_text_changed)
 
-        self.debug_buffer = self.debug_view.get_buffer()
-
-        self.text_scroll.add(self.text_view)
+        self.source_scroll.add(self.source_view)
         self.debug_scroll.add(self.debug_view)
-        self.add1(self.text_scroll)
+        self.add1(self.source_scroll)
         self.add2(self.debug_scroll)
     
     @property
     def text_iter(self, pos=None):
-        iters = self.text_buffer.get_start_iter(), self.text_buffer.get_end_iter()
+        iters = self.source_buffer.get_start_iter(), self.source_buffer.get_end_iter()
         if pos:
             return iters[pos]
         return iters
@@ -52,22 +70,22 @@ class Editor(Gtk.Paned):
         return iters
 
     def on_text_changed(self, widget):
-        text = self.text_buffer.get_text(self.text_iter[0], self.text_iter[1], False)
+        text = self.source_buffer.get_text(self.text_iter[0], self.text_iter[1], False)
         validation = Debugger.validate_yaml(text)
         current = self.app.storage.get_current_file()
-        self.text_view.do_show_completion(self.text_view)
+        self.source_view.do_show_completion(self.source_view)
         
         '''
-        cursor_iter = self.text_buffer.get_iter_at_mark(self.text_buffer.get_insert())
+        cursor_iter = self.source_buffer.get_iter_at_mark(self.source_buffer.get_insert())
         start_iter = cursor_iter.copy()
         start_iter.backward_word_start()
-        word = self.text_buffer.get_text(start_iter, cursor_iter, False)
+        word = self.source_buffer.get_text(start_iter, cursor_iter, False)
 
         if len(word) > 2:
             suggestions = self.intellisense.get_suggestions(word)
             if len(suggestions) > 0:
-                coords = self.text_view.get_iter_location(cursor_iter)
-                self.intellisense_popover.set_suggestions(suggestions, self.text_view, coords)
+                coords = self.source_view.get_iter_location(cursor_iter)
+                self.intellisense_popover.set_suggestions(suggestions, self.source_view, coords)
         '''
         
         if current:
@@ -80,13 +98,13 @@ class Editor(Gtk.Paned):
         self.app.toolbar.knock()
     
     def set_text(self, text):
-        self.text_buffer.set_text(text)
-        self.text_buffer.set_modified(False)
-        self.text_view.set_cursor_visible(True)
-        self.text_view.grab_focus()
+        self.source_buffer.set_text(text)
+        self.source_buffer.set_modified(False)
+        self.source_view.set_cursor_visible(True)
+        self.source_view.grab_focus()
     
     def get_text(self):
-        return self.text_buffer.get_text(self.text_iter[0], self.text_iter[1], False)
+        return self.source_buffer.get_text(self.text_iter[0], self.text_iter[1], False)
     
     def knock(self):
         current = self.app.storage.get_current_file()
